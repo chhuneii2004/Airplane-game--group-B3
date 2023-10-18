137656fe058227eab9e150e355ebb43cea86086f
CEO of Bathroom <141013479+chhuneii2004@users.noreply.github.com>
2023-10-18 16:09:35 +0700

    new picture change

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 root = tk.Tk()
-root.geometry("600x6600")
+root.geometry("600x900")
 frame = tk.Frame()
 frame.master.title("Skyfall")
 canvas = tk.Canvas(frame)
 
-bg = Image.open("images/background.jpg")
+bg = Image.open("images/starbackground.png")
 image = ImageTk.PhotoImage(bg)
-canvas.create_image(300,150, image=image)
+canvas.create_image(300,300, image=image)
 
 canvas.pack(expand=True, fill='both')
 frame.pack(expand=True, fill='both')
